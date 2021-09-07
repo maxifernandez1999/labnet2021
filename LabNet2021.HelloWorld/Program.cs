88a72296525bbfdf83d0fc0ce8bb188c7dc2fd35
maxifernandez1999 <fmaximiliano443@gmail.com>
2021-09-07 11:11:22 -0300

    Proyecto de Hello World Codigo inicial para probar el funcionamiento de Git y Visual Studio

---
+++ LabNet2021.HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LabNet2021.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
